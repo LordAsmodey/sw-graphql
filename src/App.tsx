8a4c26931db551953c68e270c433396304c399eb
Oleg <oleg94038@gmail.com>
2024-03-08 16:05:29 +0100

    Instaled react and apollo-client

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {gql, useQuery} from "@apollo/client";
+
+const GET_FILMS = gql`
+    query Query {
+        allFilms {
+            films {
+                title
+                planetConnection {
+                    planets {
+                        name
+                        population
+                    }
+                }
+                director
+                releaseDate
+                producers
+                speciesConnection {
+                    species {
+                        name
+                        classification
+                        homeworld {
+                            name
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+function App() {
+  const [count, setCount] = useState(0)
+  const { data } = useQuery(GET_FILMS);
+  console.log(data)
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+import { InMemoryCache, ApolloClient, HttpLink, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+)
